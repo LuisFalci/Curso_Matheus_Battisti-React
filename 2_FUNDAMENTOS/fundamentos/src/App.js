71905ec3f8e75ca29e6d3ed373897a9ee7bedc4f
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-06-12 01:40:14 -0300

    Criação de Componente

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import FirstComponent from './components/FirstComponent';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos</h1>
+     <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
