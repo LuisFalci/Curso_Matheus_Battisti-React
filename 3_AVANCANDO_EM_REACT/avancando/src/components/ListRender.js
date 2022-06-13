b27ca1a96486449746ccffe2b4e4723e9ca2f410
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-06-12 22:17:52 -0300

    Previous State

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.js
@@ -4,12 +4,20 @@ const ListRender = () =>{
 
     const [list] = useState(["Arroz", "Feijão", "Farofa", "Picanha"]);
 
-    const [users] = useState([
+    const [users, setUsers] = useState([
         { id: 1, name: "Luis", age: 32},
         { id: 2, name: "Pedro", age: 11},
         { id: 3, name: "Igor", age: 42},
     ]);
-        
+    
+    // Previous pega o estado anterior
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4);
+
+        setUsers((prevUsers) => {
+            return prevUsers.filter((user) => randomNumber !== user.id);
+        });
+    };
     
 
     // Para não ocorrer erro, utilizar o índice como parâmetro para a chave (terá um valor diferente para cada item da lista)
@@ -23,6 +31,7 @@ const ListRender = () =>{
                 {users.map((user) =>
                 <li key={user.id}>{user.name} - {user.age}</li>)}
             </ul>
+            <button onClick={deleteRandom}>Delete radom user</button>
         </div>
     )
 }
