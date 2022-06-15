0d33a230913aa687225ec49a5d7da72d50d8fc9b
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-06-15 20:42:58 -0300

    Destructuring Props (Melhor forma de usar props)

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -5,6 +5,7 @@ import ListRender from './components/ListRender';
 import ConditionalRender from './components/ConditionalRender';
 import ShowUserName from './components/ShowUserName';
 import { useState } from 'react';
+import CarDetails from './components/CarDetails';
 
 function App() {
   const name = "Gabriel"
@@ -22,6 +23,7 @@ function App() {
       <ShowUserName name={name}/>
       <ShowUserName name={userName}/>
       
+      <CarDetails brand="Fiat" km="10000" color="Vermelho" />
     </div>
 
   );
