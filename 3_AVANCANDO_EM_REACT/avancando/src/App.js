8a558efed2e70e715157008daaabc873650395f7
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-06-15 22:11:59 -0300

    Renderização de Lista em Componentes

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -8,8 +8,13 @@ import { useState } from 'react';
 import CarDetails from './components/CarDetails';
 
 function App() {
-  const name = "Gabriel"
-  const [userName] = useState("Maria")
+  const name = "Gabriel";
+  const [userName] = useState("Maria");
+  const cars = [
+    {id: 1, brand: "Ferrari", color: "Amarela", newCar: true, km: 0},
+    {id: 2, brand: "Ferrari", color: "Branco", newCar: false, km: 2323},
+    {id: 2, brand: "Ferrari", color: "Amarela", newCar: false, km: 4444},
+  ]
 
   return (
     <div className="App">
@@ -24,6 +29,12 @@ function App() {
       <ShowUserName name={userName}/>
       
       <CarDetails brand="Fiat" km="10000" color="Vermelho" />
+      <CarDetails brand="Fiat" km="10000" color="Vermelho" />
+      {/* loop em array de objetos */}
+
+      {cars.map((car) => (
+        <CarDetails brand={car.brand} color={car.color} km={car.km} newCar={car.newCar} />
+      ))}
     </div>
 
   );
