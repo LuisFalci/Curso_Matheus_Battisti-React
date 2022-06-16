1210901ee3ad21ec6663c591b5cb98f3876baf41
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-06-16 20:16:22 -0300

    Criando Formulários

---
+++ 5_FORM_EM_REACT/form/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MyForm from './components/MyForm';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Forms</h2>
+      <MyForm />
+    </div>
+  );
+}
+
+export default App;
