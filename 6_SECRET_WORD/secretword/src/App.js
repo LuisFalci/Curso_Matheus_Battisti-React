fc7444a6a3bc1b67cdca5c6444e80b2f961b0d8b
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-06-17 21:11:23 -0300

    Projeto Secret Word

---
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -0,0 +1,17 @@
+// components
+// import StartScreen from "./components/StartScreen";
+// import Game from "./components/Game";
+// import GameOver from "./components/GameOver";
+
+// styles '
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+     <h2>Secret Word</h2>
+    </div>
+  );
+}
+
+export default App;
