5923b0d1b0a32c3313fad6eeb2fb4f486e9855bf
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-06-19 15:26:15 -0300

    Carregando Dados

--- 8_REACT_ROUTER/reactrouter/src/pages/Home.js
+++ 8_REACT_ROUTER/reactrouter/src/pages/Home.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import {Link} from "react-router-dom"
+import { useFetch } from "../hooks/useFetch"
+
+import "./Home.css"
 
 const Home = () => {
+   // 3 - carregamento de dados
+   const url = "http://localhost:3000/products";
+
+   const { data: items, loading, error } = useFetch(url);
+
   return (
-    <div>Home</div>
+    <div>
+    <h1>Produtos</h1>
+    {loading && <p>Carregando dados...</p>}
+    {error && <p>{error}</p>}
+    <ul className="products">
+      {items &&
+        items.map((product) => (
+          <li key={product.id}>
+            <h2>{product.name}</h2>
+            <p>R$: {product.price}</p>
+        
+          </li>
+        ))}
+    </ul>
+  </div>
   )
 }
 
