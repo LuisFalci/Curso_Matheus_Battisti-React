c4bc308f81a90bc16b3de2ea070efa66564bca31
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-06-19 19:12:40 -0300

    Link Ativo (NavLink)

--- 8_REACT_ROUTER/reactrouter/src/components/Navbar.js
+++ 8_REACT_ROUTER/reactrouter/src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import './Navbar.css'
 
 const navbar = () => {
   return (
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">Sobre</Link>
+        {/* <Link to="/">Home</Link>
+        <Link to="/about">Sobre</Link> */}
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">Sobre</NavLink>
     </nav>
   )
 }
