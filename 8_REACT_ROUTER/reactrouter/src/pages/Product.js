a4b9ad56b6472feca022ecb25265ce0af1195e9f
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-06-19 15:37:42 -0300

    Carregamento dinâmico de dados

--- 8_REACT_ROUTER/reactrouter/src/pages/Product.js
+++ 8_REACT_ROUTER/reactrouter/src/pages/Product.js
@@ -1,13 +1,29 @@
 // useParams pega o nome passado da url. Neste caso pegamos o nome do id
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+
+import { useFetch } from "../hooks/useFetch";
 
 const Product = () => {
-  //4  - rota dinamica 
+  //4  - rota dinamica
   const { id } = useParams();
+
+  //5 - carregamento de dados
+  const url = "http://localhost:3000/products/" + id;
+
+  const { data: product, loading, error } = useFetch(url);
   return(
-    <div>
-        Product: {id}
-    </div>
+    <>
+      <p>ID do produto: {id}</p>
+
+      {error && <p>Ocorreu um erro...</p>}
+      {loading && <p>Carregando produto...</p>}
+      {product && (
+        <div>
+          <h1>{product.name}</h1>
+          <p>R${product.price}</p>
+        </div>
+      )}
+    </>
   )
 };
 
