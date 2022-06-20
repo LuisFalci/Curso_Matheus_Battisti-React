1fa15d23f4aaca591d7daf883613b1dbc2c05814
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-06-20 18:20:39 -0300

    Refatorando contexto para hook

--- 9_CONTEXT/context/src/pages/Home.js
+++ 9_CONTEXT/context/src/pages/Home.js
@@ -1,10 +1,15 @@
 import "./Home.css"
-import React, { useContext } from 'react'
-import { CounterContext } from "../context/CounterContext"
+// import React, { useContext } from 'react'
+// import { CounterContext } from "../context/CounterContext"
 import ChangeCounter from "../components/ChangeCounter"
+import { useCounterContext } from "../hooks/useCounterContext"
+
+// 4 - refatorando com hook
+
 
 const Home = () => {
-  const {counter} = useContext(CounterContext)
+  // const {counter} = useContext(CounterContext)
+  const {counter} = useCounterContext()
 
   return (
     <div>
