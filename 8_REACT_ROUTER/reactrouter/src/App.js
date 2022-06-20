3aa9945d1908abd6115155b66d1674851d022140
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-06-19 22:10:49 -0300

    Estrutura Projeto

--- 8_REACT_ROUTER/reactrouter/src/App.js
+++ 8_REACT_ROUTER/reactrouter/src/App.js
@@ -32,6 +32,8 @@ function App() {
           <Route path="/products/:id" element={<Product />} />
           {/* 9 search params */}
           <Route path="/search" element={<Search />} />
+          {/* 10 - redirect */}
+          <Route path="/company" element={<Navigate to="/about" />} />
           {/* 7  - no match route */}
           <Route path="*" element={<NotFound />} />
         </Routes>
