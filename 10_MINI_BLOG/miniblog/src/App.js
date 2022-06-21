ca51e303cd8c89e8026b3ff960a7b5a3774171b5
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-06-20 22:29:30 -0300

    React Router + Componente Navbar

--- 10_MINI_BLOG/miniblog/src/App.js
+++ 10_MINI_BLOG/miniblog/src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+// CSS
+import "./App.css";
+
+// REACT
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// PAGES
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+
+// COMPONENTS
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+
 
 function App() {
   return (
     <div className="App">
       <h1>MiniBlog</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
