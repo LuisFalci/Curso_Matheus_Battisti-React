07f594ea9c617891976d08449785ad2c418cf7a1
Luis Gustavo Furtuozo Falci <78694561+LuisFalci@users.noreply.github.com>
2022-06-26 23:56:08 -0300

    Update useFetch.js

--- 7_REQ_HTTP_REACT/httpreact/src/hooks/useFetch.js
+++ 7_REQ_HTTP_REACT/httpreact/src/hooks/useFetch.js
@@ -3,15 +3,20 @@ import {useState, useEffect} from 'react'
 // 4 - custom hook
 export const useFetch = (url) => {
     const [data, setData] = useState(null);
+    
     // 5 - refatorando post
     // configura método (se é post)
     const [config, setConfig] = useState(null);
+    
     // nome do método
     const [method, setMethod] = useState(null);
+   
     // sempre que alterarmos os dados, call fetch atualiza os dados
     const [callFetch, setCallFetch] = useState(null);
+    
     // 6 - loading
     const [loading, setLoading] = useState(false);
+   
     // 7 - tratando erros
     const [error, setError] = useState(null);
     const httpConfig = (data, method) => {
