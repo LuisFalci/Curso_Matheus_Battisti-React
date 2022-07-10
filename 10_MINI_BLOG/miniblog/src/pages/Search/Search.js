55aea0486b2528e48f2130c0d32cedeb5e4be36a
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-07-10 00:34:58 -0300

    Exibindo resultado da busca

--- 10_MINI_BLOG/miniblog/src/pages/Search/Search.js
+++ 10_MINI_BLOG/miniblog/src/pages/Search/Search.js
@@ -1,21 +1,36 @@
 import styles from "./Search.module.css";
-import React from 'react'
 
 // hooks
-import { useFetchDocuments } from "../../hooks/useFetchDocuments"
-import { useQuery } from "../../hooks/useQuery"
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useQuery } from "../../hooks/useQuery";
+
+// components
+import PostDetail from "../../components/PostDetail";
+import { Link } from "react-router-dom";
 
 function Search() {
-    const query = useQuery()
-    // o q é o parâmetro que atribuimos em Home (q=${query}) e referencia a parte da url pesquisada -> return Navigate(`/search?q=${query}`)
+  const query = useQuery();
+  // o q é o parâmetro que atribuimos em Home (q=${query}) e referencia a parte da url pesquisada -> return Navigate(`/search?q=${query}`)
+  const search = query.get("q");
 
-    const search = query.get("q")
+  const { documents: posts } = useFetchDocuments("posts", search);
+
   return (
-    <div>
-        <h2>Search</h2>
-        <p>{search}</p>
-        </div>
-  )
-}
+    <div className={styles.search_container}>
+      <h1>Resultados encontrados para: {search}</h1>
+      <div className="post-list">
+        {posts && posts.length === 0 && (
+          <>
+            <p>Não foram encontrados posts a partir da sua busca...</p>
+            <Link to="/" className="btn btn-dark">
+              Voltar
+            </Link>
+          </>
+        )}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+      </div>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
