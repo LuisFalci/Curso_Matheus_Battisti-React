c925398439a1c79c5a6c579de6d3b450031f329d
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-07-11 22:25:26 -0300

    Estruturando Dashboard

--- 10_MINI_BLOG/miniblog/src/pages/Dashboard/Dashboard.js
+++ 10_MINI_BLOG/miniblog/src/pages/Dashboard/Dashboard.js
@@ -1,14 +1,34 @@
-import React from 'react'
 import styles from "./Dashboard.module.css";
 
+import { Link } from "react-router-dom";
+
+// hooks
+import { useAuthValue } from "../../context/AuthContext";
+
+
 function Dashboard() {
+  const { user } = useAuthValue();
+  const uid = user.uid;
+  // post do usuario
+  const posts = [];
   return (
-    <div>
-        <h1>
-        Dashboard
-        </h1>
+    <div className={styles.dashboard}>
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <div className={styles.post_header}>
+          <p>Tem posts!</p>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
