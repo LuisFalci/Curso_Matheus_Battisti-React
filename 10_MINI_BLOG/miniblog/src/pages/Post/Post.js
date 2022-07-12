cbf8475cba8f823bdb44f8194407bd11e7e656d7
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2022-07-11 22:14:12 -0300

    Exibindo dado de post individual

--- 10_MINI_BLOG/miniblog/src/pages/Post/Post.js
+++ 10_MINI_BLOG/miniblog/src/pages/Post/Post.js
@@ -3,13 +3,22 @@ import styles from "./Post.module.css";
 
 // hooks
 import { useParams } from "react-router-dom";
+import { useFetchDocument } from "../../hooks/useFetchDocument";
 
 const Post = () => {
   const { id } = useParams();
+  const { document: post, loading } = useFetchDocument("posts", id);
 
-  return <div className={styles.post_container}>
-    <h1>Post: {id}</h1>
-  </div>;
+  return (
+    <div className={styles.post_container}>
+      {loading && <p>Carregando post...</p>}
+      {post && (
+        <>
+          <h1>{post.title}</h1>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Post;
